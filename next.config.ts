42b3b9ebb46c705ef7bb381745e6180c64a7a727
mdmhrz <razufreelance@gmail.com>
2026-02-07 20:35:23 +0600

    fix: deploy configuration implemented

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
-      },
-    ];
-  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/auth/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+  //     },
+  //   ];
+  // }
 };
 
 export default nextConfig;
