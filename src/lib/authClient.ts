e92f34ac3b4dfb75275d067e009c1954760c99b7
mdmhrz <razufreelance@gmail.com>
2026-02-18 22:13:21 +0600

    fix:

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { env } from "@/env";
 
 export const authClient = createAuthClient({
-    baseURL: env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: typeof window !== "undefined" ? window.location.origin : "",
+    // baseURL: env.NEXT_PUBLIC_FRONTEND_URL,
     basePath: "/api/auth",
     fetchOptions: {
         credentials: "include",
